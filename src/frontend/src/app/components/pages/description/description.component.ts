f23965050ca58702524e6608e479ecde41bde55b
luisito666 <luispenagos91@gmail.com>
2020-05-03 11:33:06 -0500

    Fix servername to description components

--- src/frontend/src/app/components/pages/description/description.component.ts
+++ src/frontend/src/app/components/pages/description/description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 // Environments
-import { environment } from '../../../../environments/environment'
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-description',
